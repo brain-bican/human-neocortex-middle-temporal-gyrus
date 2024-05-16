1e8e3688e525033c902080ec8566044084da2846
hkir-dev <huseyin_kir@yahoo.com>
2024-05-16 12:02:35 +0100

    tdt upgrade

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@
 #
 # See README-editors.md for more details.
 
-IMAGE=${IMAGE:-taxonomy-development-tools:0.0.18}
+IMAGE=${IMAGE:-taxonomy-development-tools:1.0.0}
 TDT_DEBUG=${TDT_DEBUG:-no}
 
 TIMECMD=
